905d8af0ee7776bd2db947e8ee0645c2b024b757
Chaelgutierrez <michael@vielsoft.com>
2017-07-19 08:41:11 +0800

    Updated project

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ var { BrowserWindow } = electron;
 // Platform ready
 app.on('ready', function(){
 	// Initialize window
-	var win  = new  BrowserWindow({ width:800,height:600 });
+	var win  = new  BrowserWindow({ width:800,height:600,icon: __dirname + '/img/icon.png'});
 	try {
 		// Load main view
 		win.loadURL('file://' + __dirname + '/index.html');
 		// Load Debugger
-		win.webContents.openDevTools();
+		// win.webContents.openDevTools();
 	}
 	catch(err){
 		// Log error Loading the  main window
